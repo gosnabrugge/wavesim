77f84224654d234c9686ad050ca3b695f5a322c4
jiruuino <g.osnabrugge@utwente.nl>
2016-07-18 16:05:04 +0200

    Added core scripts

---
+++ PSTD.m
@@ -0,0 +1,120 @@
+classdef PSTD < simulation
+    %Simulation of the 2-D wave equation using PSTD
+    % Saroch Leedumrongwatthanakun 2015
+    
+    properties
+        %optiond:
+        dt_relative = 0.95; %Relative size of a time step compared to the convergence criterion for PSTD. Decrease for more accurate results (default=0.95)
+        source_amplitude = @PSTD.default_source; %amplitude of the source as a function of time. Can be replaced by a different function by the user
+        
+        %internal
+        c1;   % coefficients
+        c2;
+        c3;
+        dt; % time step
+        omega; %2pi/lambda (c===1)
+        koperator; % k domain laplacian
+        dtmax; %maximum time step at sutible pixel_size
+    end
+    
+    methods
+        function obj = PSTD(sample, options)
+            %% Constructs a pseudo spectral time domain simulation object
+            %	sample = SampleMedium object
+            %   options.wavelength = free space wavelength (same unit as pixel_size, e. g. um)
+            %   options.dt         = time step (leave empty unless forcing a specific value)
+            %   options.time_duration = time duration of whole simulation
+            obj@simulation(sample, options);
+            fftw('planner','patient'); %optimize fft2 and ifft2 at first use
+            
+            %% Calculate time step dt
+            % The light speed is defined as 1 distance_unit / time_unit.
+            %
+            obj.dtmax = 2/sqrt(sample.grid.dimension)/pi*sample.grid.dx*sqrt(sample.e_r_min); %Stability condition (ref needed)
+            obj.dt = obj.dt_relative * obj.dtmax;
+            obj.iterations_per_cycle = obj.lambda / obj.dt; %lambda[distance] / dt[time] / c[distance/time]
+            
+            %% Initialize coefficients (could be optimized);
+            obj.omega = 2*pi/obj.lambda; %wave speed c_0 = 1 distance unit / time unit by definition, so omega=k00
+            c2dt = obj.dt^2./real(sample.e_r); %(relative wave speed * dt) ^2
+            sdt  = obj.dt*obj.omega*imag(sample.e_r)./real(sample.e_r); %sigma dt
+            
+            obj.c1 = (sdt-2)./(sdt+2);
+            obj.c2 = 4./(sdt+2);
+            obj.c3 = 2*c2dt./(sdt+2);
+            
+            %% Calculate PSTD laplace operator
+            obj.koperator = -p2(sample.grid);
+            obj.max_cycles = obj.max_cycles+100; %slow starting source
+            
+            if obj.gpu_enabled
+                obj.c1 = gpuArray(obj.c1);
+                obj.c2 = gpuArray(obj.c2);
+                obj.c3 = gpuArray(obj.c3);
+                obj.koperator = gpuArray(obj.koperator);
+            end
+        end
+        
+        function state = run_algorithm(obj, state)
+            %% Allocate memory for calculations
+            state.E = data_array(obj);
+            E_prev = data_array(obj);
+            A = 1; %source amplitude
+            %todo: gpuarray for c1,c2,c3 and koperator
+            %% iterate algorithm
+            while state.has_next
+                %% Calculate source amplitude
+                % First calculate time in number of optical cycles:
+                % The light speed is defined as 1 distance_unit / time_unit.
+                % Therefore, to convert iteration number to optical cycle:
+                %  Ncycle = Nit * dt[time_unit] / lambda[distance_unit] 
+                %  * c=1[distance_unit/time_unit]
+                %
+                Ncycle = state.it / obj.iterations_per_cycle;
+                A_prev = A;
+                A = obj.source_amplitude(Ncycle);
+
+                %% update fields
+                %based on equation:
+                % nabla^2 E - e_r d^2 E/dt^2 = -source
+                %
+                %approximate time derivative by centered finite difference
+                %
+                % nabla^2 E - e_r (E_next - 2*E + E_prev) / dt^2 = -source
+                %
+                % isolate E_next:
+                % E_next = (nabla^2 E + source) * dt^2/e_r + 2*E - E_prev
+                E_next = obj.c2.*state.E + obj.c1.*E_prev + obj.c3 .* (ifftn(obj.koperator.*fftn(state.E)) + A*state.source);
+                
+                if state.calculate_energy
+                    phase_shift = exp(1.0i*(angle(A)-angle(A_prev))); %expected phase shift for single step (only works for CW source!!)
+                    state.last_step_energy = wavesim.energy(E_next(obj.roi{1}, obj.roi{2}, obj.roi{3}) - state.E(obj.roi{1}, obj.roi{2}, obj.roi{3})*phase_shift);
+                    if (A<0.5) %workaround: don't terminate when source is still spinning ups
+                        state.last_step_energy = max(state.last_step_energy, state.threshold*2);
+                    end;
+                end
+                %% update fields
+                E_prev = state.E;
+                state.E = E_next;
+                state = next(obj, state);
+            end
+            
+            %finally, compensate for phase of source
+            A_next = obj.source_amplitude(state.it / obj.iterations_per_cycle);
+            state.E = state.E * exp(-1.0i*angle(A_next));
+        end
+    end
+    methods(Static)
+        function A = default_source(Ncycle)
+            %represents a CW source with a frequency corresponding to 3e8/lambda
+            %the source is switched on slowly with a cosine profile (currently takes 100 cycles)
+            %% calculate source amplitude
+            A = exp(-2.0i*pi * Ncycle);
+            %implement smooth start
+            A = A .* (0.5-0.5*cos(pi*min(Ncycle/100.0,1.0)));
+        end
+    end
+end
+
+
+
---
+++ simulation.m
@@ -0,0 +1,188 @@
+classdef simulation
+    %Base class for a 2-D wave simulation
+    % Ivo M. Vellekoop 2015
+    
+    properties
+        grid; %simgrid object
+        roi; %position of simulation area with respect to padded array
+        x_range; %x-coordinates of roi
+        y_range; %y-coordinates of roi
+        z_range; %z-coordinates of roi
+        lambda = 1; %wavelength (default = 1 unit)      
+        differential_mode = false; %when set to 'true', only the differential field for each iteration is calculated: the fields are not added to get a solution to the wave equation (used for debugging)
+        gpu_enabled = false; % flag to determine if simulation are run on the GPU (default: false)
+        callback = @simulation.default_callback; %callback function that is called for showing the progress of the simulation. Default shows image of the absolute value of the field.
+        callback_interval = 50; %the callback is called every 'callback_interval' steps. Default = 5
+        energy_threshold = 1E-20; %the simulation stops when the difference for a step is less than 'energy_threshold'
+        energy_calculation_interval = 10; %only calculate energy difference every N steps (to reduce overhead)
+        max_cycles = 0; %number of wave periods to run the simulation. The number of actual iterations per cycle depends on the algorithm and its parameters
+        %internal:
+        iterations_per_cycle;%must be set by derived class
+    end
+    
+    methods
+        function obj=simulation(sample, options)
+            %% Constructs a simulation object
+            %	sample = SampleMedium object
+            %   options = simulation options. 
+            %   options.max_cycles = maximum number of optical cycles for
+            %   which to run the simulation. Note that the number of
+            %   required iterations per cycle depends on the algorithm and
+            %   its parameters
+
+            %copy values from 'options'to 'obj' (only if the field is present in obj)
+            obj_fields = fields(obj);
+            opt_fields = fields(options);
+            fs = intersect(obj_fields, opt_fields);
+            for f=1:length(fs)
+                fieldname = fs{f};
+                obj.(fieldname) = options.(fieldname);
+            end
+            
+            obj.grid = sample.grid;
+            obj.roi  = sample.roi;
+            obj.x_range = sample.grid.x_range(obj.roi{2});
+            obj.x_range = obj.x_range - obj.x_range(1);
+            obj.y_range = sample.grid.y_range(obj.roi{1});
+            obj.y_range = obj.y_range - obj.y_range(1);
+            obj.z_range = sample.grid.z_range(obj.roi{3});
+            obj.z_range = obj.z_range - obj.z_range(1);
+            if (obj.max_cycles == 0) %default: 1.5 pass
+                LN = sqrt(length(obj.x_range).^2 + length(obj.y_range)^2 + length(obj.z_range)^2);
+                obj.max_cycles = LN * obj.grid.dx / obj.lambda * 2;
+            end
+        end
+        
+        function [E, state] = exec(obj, source)
+            tic;
+            
+            %%% prepare state (contains all data that is unique to a single 
+            % run of the simulation)
+            %
+            state.it = 1; %iteration
+            state.max_iterations = ceil(obj.max_cycles * obj.iterations_per_cycle);
+            disp(state.max_iterations);
+            state.diff_energy = zeros(state.max_iterations,1);
+            state.threshold = obj.energy_threshold * simulation.energy(source); %energy_threshold = fraction of total input energy
+            state.last_step_energy = inf;
+            state.calculate_energy = true;
+            state.has_next = true;
+            
+            %% increase source array (which currently has the size of the roi)
+            % to the full grid size (including boundary conditions)
+            if (issparse(source) && ~obj.gpu_enabled)
+                state.source = sparse(obj.grid.N(1), obj.grid.N(2));
+            else
+                state.source = data_array(obj);
+            end;
+            if (obj.grid.dimension == 2)
+                state.source(obj.roi{1}, obj.roi{2}) = source;
+            else
+                state.source(obj.roi{1}, obj.roi{2}, obj.roi{3}) = source;
+            end    
+            %%% Execute simulation
+            % the run_algorithm function should:
+            % - update state.last_step_energy when required
+            % - call next(obj, state)
+            % - return calculated field in state.E
+            %(see wavesim for an example)
+            state = run_algorithm(obj, state);
+            
+            state.time=toc;
+            if state.converged
+                disp(['Reached steady state in ' num2str(state.it) ' iterations']);
+                disp(['Time consumption: ' num2str(state.time) ' s']);
+            else
+                disp('Did not reach steady state');
+            end
+            E = state.E(obj.roi{1}, obj.roi{2}, obj.roi{3}); %% return only part inside roi. Array remains on the gpu if gpuEnabled = true
+        end;
+        
+        %% Creates an array of dimension obj.grid.N. If gpuEnabled is true, the array is created on the cpu
+        function d = data_array(obj)
+            %% Check whether gpu computation option is enabled
+            if obj.gpu_enabled
+                d = gpuArray(obj.grid.Nred);
+            else
+                d = zeros(obj.grid.Nred);
+            end
+        end;
+        
+        %% Continue to the next iteration. Returns false to indicate that the simulation has terminated
+        function state = next(obj, state)
+            %% store energy
+            state.diff_energy(state.it) = state.last_step_energy;
+            
+            %% check if simulation should terminate
+            if (state.last_step_energy < state.threshold)
+                state.has_next = false;
+                state.converged = true;
+            elseif (state.it >= state.max_iterations)
+                state.has_next = false;
+                state.converged = false;
+            else
+                state.has_next = true;
+            end;
+            
+            %% do we need to calculate the last added energy? (only do this once in a while, because of the overhead)
+            state.calculate_energy = mod(state.it, obj.energy_calculation_interval)==0;
+            
+            %% call callback function if neened
+            if (mod(state.it, obj.callback_interval)==0 || ~state.has_next) %now and then, call the callback function to give user feedback
+                obj.callback(obj, state);
+            end
+            state.it = state.it+1;
+            
+            %% For the differential mode, shut down the source after iteration 1
+            if (obj.differential_mode)
+                state.source = 0;
+            end
+        end
+    end
+    
+    methods(Static)
+        function en = energy(E_x)
+            en= sum(abs(E_x(:)).^2);
+        end
+        
+        function abs_image_callback(obj, state)
+            figure(1);
+            E = state.E;
+            zpos = ceil(size(E, 3)/2);
+            imagesc(abs(E(:,:,zpos)));
+            title(['Differential energy ' num2str(state.diff_energy(state.it))]);
+            drawnow;
+        end
+        
+        %default callback function. Shows real value of field, and total energy evolution
+        function default_callback(obj, state)
+            figure(1);
+            energy = state.diff_energy(1:state.it);
+            threshold = state.threshold;
+            E = state.E;
+            subplot(2,1,1); plot(1:length(energy),log10(energy),'b',[1,length(energy)],log10(threshold)*ones(1,2),'--r');
+            title(length(energy));  xlabel('# iterations'); ylabel('log_{10}(energy added)');
+            
+            % plot midline along longest dimension
+            xpos = ceil(size(E, 2)/2);
+            ypos = ceil(size(E, 1)/2);
+            zpos = ceil(size(E, 3)/2);
+            if xpos > max(ypos, zpos)
+                sig = log(abs(E(ypos, :, zpos)));
+                sroi = obj.roi{2};
+            elseif ypos > max(xpos, zpos)
+                sig = log(abs(E(:, xpos, zpos)));
+                sroi = obj.roi{1};
+            else
+                sig = log(abs(E(ypos, xpos, :)));
+                sroi = obj.roi{3};
+            end
+            subplot(2,1,2); plot(1:length(sig), squeeze(sig), sroi(1)*ones(1,2), [min(sig),max(sig)], sroi(end)*ones(1,2), [min(sig),max(sig)]);
+            title('Cross-section')
+            xlabel('y (\lambda / 4)'); ylabel('log_{10}(|E|)');
+            
+            %disp(['Added energy ', num2str(energy(end))]);
+            drawnow;
+        end
+    end
+end
